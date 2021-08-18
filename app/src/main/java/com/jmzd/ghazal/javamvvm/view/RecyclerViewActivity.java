cfb0473d733584d1195d9c5d2cb2cf7c8dd0abe9
Your Name <jmzd.developer94@gmail.com>
2021-08-18 19:05:41 +0430

    Back Stack

--- app/src/main/java/com/jmzd/ghazal/javamvvm/view/RecyclerViewActivity.java
+++ app/src/main/java/com/jmzd/ghazal/javamvvm/view/RecyclerViewActivity.java
@@ -3,6 +3,8 @@ package com.jmzd.ghazal.javamvvm.view;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -10,17 +12,23 @@ import androidx.navigation.ui.NavigationUI;
 import com.jmzd.ghazal.javamvvm.R;
 
 public class RecyclerViewActivity extends AppCompatActivity {
-NavController navController ;
+    NavController navController;
+    Fragment fragment; // back -> ما را از back stack thread خارج می کند.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
-        navController= Navigation.findNavController(this , R.id.mainFragment);
+
+        fragment = getSupportFragmentManager().findFragmentById(R.id.mainFragment);//back
+
+        navController = Navigation.findNavController(this, R.id.mainFragment);
         NavigationUI.setupActionBarWithNavController(this, navController);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        return super.onSupportNavigateUp();
+      //  return super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, (DrawerLayout) null);//back // ما اینجا DrawerLayout نداریم ولی گر داشتیم همینجا ازش استفاده میکردیم.
     }
 }
